1050390db19820b2a62e62d5513fab83d2982856
prashantkhandelwal <prashant@midnightprogrammer.net>
2022-07-13 20:47:37 +0530

    Update NewScrapper function.

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-func main() {
-
-	// Initialize Colly
-	c := colly.NewCollector(
+func NewScrapper() *colly.Collector {
+	return colly.NewCollector(
 		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
 		colly.CacheDir("cache"),
 		colly.Async(true),
 	)
+}
 
+func main() {
+
+	// Initialize Colly
 	t20_downloader := c.Clone()
 	t20matchresult := T20MatchResult{}
 
